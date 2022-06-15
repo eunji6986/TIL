9230b588ed968c3762635459269e16ccba7fe7f8
LEE EUNJI <eunji6986@naver.com>
2022-06-15 23:51:29 +0900

    # stream example

---
+++ src/main/java/com/example/til/stream/Stream1.java
@@ -0,0 +1,18 @@
+package com.example.til.stream;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+public class Stream1 {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>(Arrays.asList("a", "b", "c","a", "b","c"));
+
+        String[] array = new String[]{"a", "b", "c","a", "b","c"};
+        Stream<String> stream1 = Arrays.stream(array);
+        Stream<String> stream2 = Arrays.stream(array, 2, 5);
+
+        stream1.forEach(System.out::println);
+        stream2.forEach(System.out::print);
+    }
+}
